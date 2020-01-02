d1135e9052b220480970b94caf3bdf7c9382d067
amarjitthakur <amarjit2954@gmail.com>
2020-01-02 16:05:32 +0530

    Updates

---
+++ precache-manifest.6792ee8e754c5280299ecb1685f5d4bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7960a8c65af86a1b20c27ad2bbda2b41",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "3b96a3edb3ea397c3eeb",
+    "url": "/contactmanager/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "d3c85918f2a094848d32",
+    "url": "/contactmanager/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3b96a3edb3ea397c3eeb",
+    "url": "/contactmanager/static/js/2.8b3a0dea.chunk.js"
+  },
+  {
+    "revision": "d3c85918f2a094848d32",
+    "url": "/contactmanager/static/js/main.1106af7b.chunk.js"
+  },
+  {
+    "revision": "52a8038624ffe69f6d88",
+    "url": "/contactmanager/static/js/runtime-main.9acb0f81.js"
+  }
+]);
